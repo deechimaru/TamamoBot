a17c0d94a2f39b0400f007076e9c260df16e3be8
deechimaru <deechimaru@gmail.com>
2019-06-22 00:35:10 +0200

    Updated bot's help command

--- bot.js
+++ bot.js
@@ -43,7 +43,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             case 'help':
                 bot.sendMessage({
                     to: userID,
-                    message: "Hi! I'm Tamamo, custom discord bot made by D-Ecchi. `I'm currently in Alpha stage, so everything can happen.` These are current commands [use & prefix]:```fox: sends a wild Tamamo! \nping: pings (in case of lag etc.) \nallow: makes certain channel Tamamo-able! \nhelp: help!```"
+                    message: "Hi! I'm Tamamo, custom discord bot made by D-Ecchi. `I'm currently in Alpha stage, so everything can happen.` These are current commands [use & prefix]:```tamamo: sends Danbooru image \nfox: sends a wild Tamamo (soon to be deleted)! \nping: pings (in case of lag etc.) \nallow: makes certain channel Tamamo-able! \nhelp: help!```"
                 })
             break;
             case 'fox':
